d4d61c47760ad9c4fcfc815f5fb96213cfe035d0
Viraj Trivedi <inf3cti0n95@gmail.com>
2026-06-01 10:58:38 +0530

    Initial commit of decoupled commerce module

---
+++ order/constants.go
@@ -0,0 +1,22 @@
+package order
+
+// Order statuses.
+const (
+	StatusPending   = "PENDING"
+	StatusPaid      = "PAID"
+	StatusFulfilled = "FULFILLED"
+	StatusCancelled = "CANCELLED"
+)
+
+// Event types.
+const (
+	EventOrderCreated = "order.created"
+	EventOrderPaid    = "order.paid"
+)
+
+// Workflow task names.
+const (
+	TaskCreateOrder       = "order.create"
+	TaskFinalizeOrder     = "order.finalize"
+	TaskCompensatePayment = "order.compensate_payment"
+)
